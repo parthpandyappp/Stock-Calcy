925a6cb0c3b0b767a8797f1245a3a0e3b1ea46f7
Parth Pandya <34797335+parthpandyappp@users.noreply.github.com>
2021-08-29 16:47:11 +0530

    Added JavaScript support Form handling by JavaScript is fixed. Calculations and other relevant functions are defined.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-function testing() {
-  console.log("Click here!");
+var initial = document.querySelector("#initial-price");
+
+var quantity = document.querySelector("#quantity");
+
+var current = document.querySelector("#current-price");
+
+var res = document.querySelector("#res");
+
+function calculate() {
+  let Profit = Math.abs(+current.value - +initial.value) * +quantity.value;
+
+  res.innerHTML = "Profit is: " + Profit;
 }
