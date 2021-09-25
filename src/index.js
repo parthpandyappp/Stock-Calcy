401d5f6ea4d8e9daf50d5b2146a87e28f776fa84
parthpandyappp <parthpandyappp@gmail.com>
2021-09-25 22:44:12 +0530

    Updated the [index.js] to display percentage

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
-var initial = document.querySelector("#initial-price");
-
-var quantity = document.querySelector("#quantity");
-
-var current = document.querySelector("#current-price");
-
 var res = document.querySelector("#res");
 
-function calculate() {
-  let Profit = Math.abs(+current.value - +initial.value) * +quantity.value;
+function formHandler() {
+  var initial = document.querySelector("#initial-price");
 
-  res.innerHTML = "Profit made on the stock is: " + Profit + " Ruppees";
+  var quantity = document.querySelector("#quantity");
+
+  var current = document.querySelector("#current-price");
+
+  calculatePAndL(initial, quantity, current);
+
+}
+
+
+function calculatePAndL(initial, quantity, current) {
+  console.log(initial.value)
+  console.log(quantity.value)
+  console.log(current.value)
+  if (Number(initial.value) > Number(current.value)) {
+    var loss = (initial.value - current.value) * quantity.value;
+    var lossPercentage = Math.abs((loss / initial.value) * 100);
+
+    var textOut = `Loss made on the stock is ${loss} and the percentage is ${lossPercentage}%`;
+    res.innerHTML = textOut;
+  } else if (Number(current.value) > Number(initial.value)) {
+    var profit = Math.abs((current.value - initial.value) * quantity.value);
+    var profitPercentage = (profit / initial.value) * 100;
+    var textOut = `Profit made on the stock is ${profit} and the percentage is ${profitPercentage}%`;
+    res.innerHTML = textOut;
+
+
+  } else {
+    res.innerHTML = "You seem to have a balanced hold";
+  }
 }
